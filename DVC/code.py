f8ba8d079fa3f9e1bfb48e88b9ac421144848d03
ashutosh15072000 <ashutoshs019@gmail.com>
2025-07-07 16:42:48 +0530

    second verison of data saved

--- DVC/code.py
+++ DVC/code.py
@@ -9,8 +9,8 @@ data={'Name':['ashu',"ram","shyam"],
 
 df = pd.DataFrame(data)
 # ## Adding new row to df for v2
-# new_row_loc = {"Name":'v2', "Age":30, "City":'Lucknow'}
-# df.loc[len(df.index)]=new_row_loc
+new_row_loc = {"Name":'v2', "Age":30, "City":'Lucknow'}
+df.loc[len(df.index)]=new_row_loc
 
 # ## Adding new row to df for v3
 # new_row_loc = {"Name":'v3', "Age":35, "City":'Lucknow'}
